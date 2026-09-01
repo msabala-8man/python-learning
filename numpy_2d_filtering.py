00d7e12d785a82465abc26dd4ac8064c0fe5e619
Mateusz <m.sabala2@student.uw.edu.pl>
2026-09-01 21:59:53 +0200

    Add comment to filtering exercise

--- numpy_2d_filtering.py
+++ numpy_2d_filtering.py
@@ -30,13 +30,13 @@ scores = np.array([
 # 9. Replace every score below 60 with 60.
 # 10. Calculate the average of scores greater than or equal to 80.
 
-print(scores[scores >= 80])
-print(scores[scores < 70])
-print(scores[(scores > 70) & (scores < 90)])
-print(scores[(scores > 90) | (scores < 60)])
-print(len(scores[scores >= 80]))
-print(len(scores[scores < 70]))
-print(scores + 5)
-scores[scores < 70] += 10
-scores[scores < 60] = 60
-print(np.mean(scores[scores >= 80]))
+print(scores[scores >= 80])                     # 1.
+print(scores[scores < 70])                      # 2.
+print(scores[(scores > 70) & (scores < 90)])    # 3.
+print(scores[(scores > 90) | (scores < 60)])    # 4.
+print(len(scores[scores >= 80]))                # 5.
+print(len(scores[scores < 70]))                 # 6.
+print(scores + 5)                               # 7.
+scores[scores < 70] += 10                       # 8.
+scores[scores < 60] = 60                        # 9.
+print(np.mean(scores[scores >= 80]))            # 10.  
